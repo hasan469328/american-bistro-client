43fc318bf08336992e43c6f76a5654b2247ae623
hasan469328 <hasanur.68.bd@gmail.com>
2023-05-04 16:17:16 +0600

    environment variable added

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBwhoqobzXfH41n2J5vse5hoAPFKWiVWo4",
-  authDomain: "americana-bistro-client.firebaseapp.com",
-  projectId: "americana-bistro-client",
-  storageBucket: "americana-bistro-client.appspot.com",
-  messagingSenderId: "219263935465",
-  appId: "1:219263935465:web:f154d254f637c82ed22178"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
