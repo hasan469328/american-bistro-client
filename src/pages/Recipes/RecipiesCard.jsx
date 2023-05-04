dfa5d4446b3f427efdb7f3fa74178ea1700cb27d
hasan469328 <hasanur.68.bd@gmail.com>
2023-05-04 09:40:53 +0600

    conditionally render disabled button

--- src/pages/Recipes/RecipiesCard.jsx
+++ src/pages/Recipes/RecipiesCard.jsx
@@ -1,14 +1,17 @@
 import { Rating } from "@smastrom/react-rating";
-import React from "react";
+import React, { useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-const notify = () => {
-  return toast("Added as Favorite!");
-};
+
 
 const RecipiesCard = ({ re }) => {
   const { cooking_method, ingredients, rating, recipe_name } = re;
+  const [btnDisable, setBtnDisable] = useState(false)
+  const notify = () => {
+    setBtnDisable(true)
+    return toast("Added as Favorite!");
+  };
   return (
     <div className="card bg-base-100 shadow-xl">
       <div className="card-body">
@@ -28,8 +31,8 @@ const RecipiesCard = ({ re }) => {
         <div className="flex items-center">
           <div className="flex-1">{rating}</div>
           <button
-            onClick={notify}
-            className="bg-gradient-to-r from-red-500 to-yellow-500 hover:from-red-600 hover:to-yellow-600 text-white  py-2 px-2 border border-red-600 rounded-lg shadow-md transition duration-300 ease-in-out font-semibold"
+            onClick={notify} 
+            className={!btnDisable ? "bg-gradient-to-r from-red-500 to-yellow-500 hover:from-red-600 hover:to-yellow-600 text-white  py-2 px-2 border border-red-600 rounded-lg shadow-md transition duration-300 ease-in-out font-semibold" : "btn btn-disabled"}
           >
             Favorite
           </button>
